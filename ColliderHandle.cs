16ea1c40d99b75dc1fee229145e6ebc76a90d1d9
ricardovsilva <ricardovsilva@msn.com>
2014-02-21 23:22:52 -0300

    Start of development

---
+++ ColliderHandle.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collision
+{
+    public class ColliderHandle : GameComponent
+    {
+        private List<ICollidable> CollidableObjects;
+
+        public ColliderHandle(Game game)
+            : base(game)
+        {
+            CollidableObjects = new List<ICollidable>();
+        }
+
+        public void AddCollidable()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            CleanList();
+            CheckCollisions();
+            base.Update(gameTime);
+        }
+
+        private void CleanList()
+        {
+            foreach(ICollidable collidable in CollidableObjects)
+            {
+                if(collidable == null)
+                {
+                    CollidableObjects.Remove(collidable);
+                }
+            }
+        }
+
+        private void CheckCollisions()
+        {
+            foreach(ICollidable currentCollidable in CollidableObjects)
+            {
+                foreach(ICollidable collidable in CollidableObjects)
+                {
+                    if(collidable != currentCollidable)
+                    {
+                        bool isColliding = false;
+                        Rectangle currenCollidableRec = currentCollidable.GetBounds();
+                        collidable.GetBounds().Intersects(ref currenCollidableRec, out isColliding);
+
+                        if(isColliding)
+                        {
+                            currentCollidable.OnCollisionEnter.Invoke(collidable, null);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ICollidable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Collision
+{
+    public class CollisionHandler : System.EventArgs
+    {
+
+    }
+
+    public interface ICollidable
+    {
+        Rectangle GetBounds();
+        event EventHandler OnCollisionEnter;
+    }
+}
